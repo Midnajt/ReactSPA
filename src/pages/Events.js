e9a92edb472eb18e5643746a66e89648bf22d896
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-03-21 15:52:09 +0100

    loader added

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,26 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  { id: "e1", title: "e1 - Woodstock" },
-  { id: "e2", title: "e2 - Poland Rock" },
-  { id: "e3", title: "e3 - Jarocin" },
-  { id: "e4", title: "e4 - Opener" },
-];
+import EventsList from "../components/EventsList";
+import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
